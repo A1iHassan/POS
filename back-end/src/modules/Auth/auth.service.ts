711933ddcfbaed0fe7801d12cf552cfa3817d8e5
A1iHassan <ali012wkout@gmail.com>
2026-05-21 18:39:35 +0300

    added hashing for sign up passwords

--- back-end/src/modules/Auth/auth.service.ts
+++ back-end/src/modules/Auth/auth.service.ts
@@ -1,9 +1,10 @@
 import { Inject, Injectable, BadRequestException } from "@nestjs/common";
 import { NodePgDatabase } from "drizzle-orm/node-postgres";
+import { eq } from "drizzle-orm";
+import { hash } from "bcrypt"
 import { dbClient } from "src/core/db/db.constants";
 import * as schema from '../../core/db/schema'
 import { NewUser } from "./presentation/dto/createUser";
-import { eq } from "drizzle-orm";
 
 @Injectable()
 export class AuthService {
@@ -14,6 +15,7 @@ export class AuthService {
     async addNewUser(payload: NewUser) {
         const users = await this.db.select().from(schema.users).where(eq(schema.users.name, payload.name))
         if (users.length > 0) throw BadRequestException
+        payload.password = hash(payload.password, 20)
         return await this.db.insert(schema.users).values(payload).returning({ name: schema.users.name })
     }
 }
