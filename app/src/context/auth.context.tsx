ac304f1f0c08cf1a3bfb53b42821358956a6562d
A1iHassan <ali012wkout@gmail.com>
2026-05-21 19:22:50 +0300

    clean up left over files

---
+++ app/src/context/auth.context.tsx
@@ -0,0 +1,13 @@
+// import { createContext, useState } from "react";
+
+// interface UserData {
+//     name: string,
+//     loggedIn: boolean
+//     // will add more user related data soon
+// }
+
+// const Auth = createContext<UserData | null>(null)
+
+// export AuthContext({children}) {
+//     return <Auth.pr
+// }
