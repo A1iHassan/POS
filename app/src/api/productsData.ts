29b5c9b2168ce0278d00b8582df7a9d847804425
A1iHassan <ali012wkout@gmail.com>
2026-05-31 20:58:55 +0300

    redirected api to live build

--- app/src/api/productsData.ts
+++ app/src/api/productsData.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const productsApi = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE || "http://localhost:3000/api/v1/products",
+    baseURL: import.meta.env.VITE_API_BASE || "https://pos-avnr.onrender.com/api/v1/products",
     headers: { "Content-Type": "application/json" }
 })
