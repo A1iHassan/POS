22635070e50c9f37ef5bd83587454c4171e5d92e
A1iHassan <ali012wkout@gmail.com>
2026-06-25 21:27:50 +0300

    redirected products api to new host domain

--- app/src/api/productsData.ts
+++ app/src/api/productsData.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const productsApi = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE || "https://pos-avnr.onrender.com/api/v1/products",
+    baseURL: import.meta.env.VITE_API_BASE || "https://pos.roarisolutions.com/api/v1",
     headers: { "Content-Type": "application/json" }
 })
