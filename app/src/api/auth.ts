76efd23eec50f95f48df0b8fe6a848db4099ffab
A1iHassan <ali012wkout@gmail.com>
2026-06-25 21:29:12 +0300

    redirected auth api to new hsot domain

--- app/src/api/auth.ts
+++ app/src/api/auth.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const authApi = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE || "http://localhost:3000/api/v1/auth",
+    baseURL: import.meta.env.VITE_API_BASE || "https://pos.roarisolutions.com/api/v1",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
