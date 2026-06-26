743eba33ab38b5999e4a7bf19b634179bf903b2b
AliHassan <ali012wkout@gmail.com>
2026-06-26 11:43:09 +0000

    correcting products end point path

--- app/src/api/productsData.ts
+++ app/src/api/productsData.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const productsApi = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE || "https://pos.roarisolutions.com/api/v1",
+    baseURL: import.meta.env.VITE_API_BASE || "https://pos.roarisolutions.com/api/v1/products",
     headers: { "Content-Type": "application/json" }
 })
